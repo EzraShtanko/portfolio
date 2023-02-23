660c82984a3aa520868e3f65a839330578a9737b
EzraShtanko <ezra.shtanko@gmail.com>
2023-02-22 20:08:27 -0500

    added stub for body background parallax effect

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 var dots = document.querySelectorAll("[id=graphic]");
+// -- TODO a localized container to content correspondance for graphic
+// -- elements of each individual project cover
+var mbckg = document.getElementById("main_parallax_background");
+// -- retrieve initial background position precribed to "main_parallax_background"
 
+console.log(mbckg);
 console.log(dots);
