406f185e31e7fe2283a8fd7c5822733dfea166d4
EzraShtanko <ezra.shtanko@gmail.com>
2023-02-23 23:00:21 -0500

    added callback for enabling parallax on main page

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+var parallax_endpos_y = -30
+var current_scroll = 0.0
+
+
+var mbckg = document.getElementById("main_parallax_background");
+var init_mbckg_pos_y = mbckg.style.backgroundPositionY;
+var parallax_span = init_mbckg_pos_y - parallax_endpos_y;
+
+
 var dots = document.querySelectorAll("[id=graphic]");
 // -- TODO a localized container to content correspondance for graphic
 // -- elements of each individual project cover
-var mbckg = document.getElementById("main_parallax_background");
-// -- retrieve initial background position precribed to "main_parallax_background"
+
 
 console.log(mbckg);
 console.log(dots);
+
+document.addEventListener("scroll", (event) => {
+// parallax functionality
+// TODO
+});
